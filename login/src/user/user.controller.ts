589909d61a5f12f1e48dc63d002ab6e55efa6b09
Nguyễn Thế Anh <89338550+Theanh2911@users.noreply.github.com>
2024-11-14 10:07:14 +0700

    Update delete account

--- login/src/user/user.controller.ts
+++ login/src/user/user.controller.ts
@@ -1,50 +1,75 @@
-import {Controller, Post, Body, Patch} from '@nestjs/common';
+import { Controller, Post, Body, Patch, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
-import {User} from "./user.entity";
+import { User } from './user.entity';
 
 @Controller('auth')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Post('register')
-    async register(
-        @Body('username') userName: string,
-        @Body('password') password: string,
-        @Body('email') email: string,
-        @Body('dateOfBirth') dateOfBirth: string,
-        @Body('nickname') nickName: string,
-    ) {
-        return this.userService.register(userName, password, email, dateOfBirth, nickName);
-    }
+  @Post('register')
+  async register(
+    @Body('username') userName: string,
+    @Body('password') password: string,
+    @Body('email') email: string,
+    @Body('dateOfBirth') dateOfBirth: string,
+    @Body('nickname') nickName: string,
+  ) {
+    return this.userService.register(
+      userName,
+      password,
+      email,
+      dateOfBirth,
+      nickName,
+    );
+  }
 
-    @Post('login-via-username')
-    async loginViaUsername(@Body() body: { userName: string; password: string }) {
-        const { userName, password } = body;
-        return await this.userService.authenticateViaUsername(userName, password)
-            ? 'Login successful!'
-            : 'Invalid username or password!';
-    }
+  @Post('login-via-username')
+  async loginViaUsername(@Body() body: { userName: string; password: string }) {
+    const { userName, password } = body;
+    return (await this.userService.authenticateViaUsername(userName, password))
+      ? 'Login successful!'
+      : 'Invalid username or password!';
+  }
 
-    @Post('login-via-email')
-    async loginViaEmail(@Body() body: { email: string; password: string }) {
-        const { email, password } = body;
-        return await this.userService.authenticateViaEmail(email, password)
-            ? 'Login successful!'
-            : 'Invalid email or password!';
-    }
+  @Post('login-via-email')
+  async loginViaEmail(@Body() body: { email: string; password: string }) {
+    const { email, password } = body;
+    return (await this.userService.authenticateViaEmail(email, password))
+      ? 'Login successful!'
+      : 'Invalid email or password!';
+  }
 
-    @Patch('update-user-info')
-    async changeInfo(
-        @Body() body: {
-            userName?: string;
-            password?: string;
-            email?: string;
-            nickName?: string;
-            avatar_url?: string;
-        }
-    ): Promise<User> {
-        const { userName, password, email, nickName, avatar_url } = body;
-        return this.userService.changeInfo(userName, password, email, nickName, avatar_url);
-    }
+  @Patch('update-user-info')
+  async changeInfo(
+    @Body()
+    body: {
+      userName?: string;
+      password?: string;
+      email?: string;
+      nickName?: string;
+      avatar_url?: string;
+    },
+  ): Promise<User> {
+    const { userName, password, email, nickName, avatar_url } = body;
+    return this.userService.changeInfo(
+      userName,
+      password,
+      email,
+      nickName,
+      avatar_url,
+    );
+  }
 
+  @Delete('update-user-info')
+  async deleteAccount(
+    @Body()
+    body: {
+      userName?: string;
+      password?: string;
+    },
+  ): Promise<string> {
+    const { userName, password } = body;
+    await this.userService.deleteAccount(userName, password);
+    return 'delete success';
+  }
 }
