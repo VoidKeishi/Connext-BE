7d28af6e80e11311f91c55882b7a1b83a2d9801e
VoidKeishi <phananha4@gmail.com>
2024-12-05 09:35:42 +0700

    topic: Codebase Auth Module

--- connext/src/auth/auth.controller.ts
+++ connext/src/auth/auth.controller.ts
@@ -1,8 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request, Response, response } from 'express';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @Post('sign-in')
+  async signin(@Body() signinData: AuthDto, @Res() response: Response) {
+    // TODO: Call signin service
+    // TODO: Set cookie for access token and refresh token
+    // TODO: Return user data
+  }
+
+  @Post('sign-up')
+  async signup(@Body() signupData: AuthDto, @Res() response: Response) {
+    // TODO: Call signup service
+    // TODO: Set cookie for access token and refresh token
+    // TODO: Return user data
+  }
+
+  @Post('logout')
+  async logout(@Req() request: Request, @Res() response: Response) {
+    // TODO: Call logout service
+    // TODO: Delete cookie key access token and refresh token
+    // TODO: Return true
+  }
 }
---
+++ connext/src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { UnauthorizedException } from '@nestjs/common'
+import * as jwt from 'jsonwebtoken'
+
+type Payload = {
+  email: string,
+  user_id: number,
+}
+
+export const jwtSign = (payload: Payload, secretKey: string, expiresIn: string) => {
+  const token = jwt.sign(payload, secretKey, { expiresIn: expiresIn })
+  return token
+}
+
+export const verifyToken = (token: string, secretKey: string) => {
+  try {
+    const decodedData = jwt.verify(token, secretKey)
+    return decodedData
+  } catch (error) {
+    throw new UnauthorizedException(error)
+  }
+}
