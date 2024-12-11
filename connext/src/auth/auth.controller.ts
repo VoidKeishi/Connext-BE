813a86d6b506d1f23230217a347bdce36f61f94c
VoidKeishi <phananha4@gmail.com>
2024-12-11 15:30:40 +0700

    fix: Reove secure property of cookie for using on local environment

--- connext/src/auth/auth.controller.ts
+++ connext/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Get, Body, Res, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Res,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request, Response } from 'express';
 import { AuthDto } from './dto/auth.dto';
@@ -6,27 +14,24 @@ import { Roles } from './roles-decorator';
 import { Role } from './role-enum';
 import { AuthGuard } from './guards/jwt-auth.guard';
 
-
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  //user
   @Post('sign-in')
   async signin(@Body() signinData: AuthDto, @Res() response: Response) {
-    const { accessToken, refreshToken, user } = await this.authService.signin(signinData);
+    const { accessToken, refreshToken, user } =
+      await this.authService.signin(signinData);
     response.cookie('accessToken', accessToken, {
       httpOnly: true,
-      secure: true, 
-      sameSite: 'none',
-      maxAge: 60 * 60 * 1000, 
+      secure: false,
+      maxAge: 60 * 60 * 1000,
     });
 
     response.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: true, 
-      sameSite: 'none',
-      maxAge: 7 * 24 * 60 * 60 * 1000, 
+      secure: false,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     return response.status(200).json({ user });
@@ -34,18 +39,17 @@ export class AuthController {
 
   @Post('sign-up')
   async signup(@Body() signupData: AuthDto, @Res() response: Response) {
-    const { accessToken, refreshToken, user } = await this.authService.signup(signupData);
+    const { accessToken, refreshToken, user } =
+      await this.authService.signup(signupData);
     response.cookie('accessToken', accessToken, {
       httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-      maxAge: 15 * 60 * 1000, 
+      secure: false,
+      maxAge: 15 * 60 * 1000,
     });
 
     response.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: true, 
-      sameSite: 'none',
+      secure: false,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -55,8 +59,7 @@ export class AuthController {
   @UseGuards(AuthGuard)
   @Post('logout')
   async logout(@Req() request: Request, @Res() response: Response) {
-
-    await this.authService.logout(request["user"].user_id);
+    await this.authService.logout(request['user'].user_id);
 
     response.clearCookie('accessToken');
     response.clearCookie('refreshToken');
