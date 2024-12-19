dcc68c80f522629f4c339b736c667a004e52b141
VoidKeishi <phananha4@gmail.com>
2024-12-20 02:15:52 +0700

    fix: Cors when deploying

--- connext/src/common/constants/allow-list.contant.ts
+++ connext/src/common/constants/allow-list.contant.ts
@@ -1,1 +1,6 @@
-export const ALLOW_LIST = ['http://localhost:3000', 'http://localhost:8085'];
+export const ALLOW_LIST = [
+  'http://localhost:3000',
+  'http://localhost:8085',
+  'https://connext-fe.vercel.app/',
+  'https://connext-fe.vercel.app',
+];
