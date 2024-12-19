9937df9b0f4d86cd93a736df1506bc7051a36d3e
VoidKeishi <phananha4@gmail.com>
2024-12-20 00:17:37 +0700

    feature: Get all users api

--- connext/src/dashboard/dashboard.controller.ts
+++ connext/src/dashboard/dashboard.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req, Query } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { AuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { Request } from 'express';
 import { Roles } from 'src/auth/roles-decorator';
 import { Role } from 'src/common/enum/role-enum';
 import { RolesGuard } from 'src/auth/guards/roles-guard';
+import { GetUserPaginatedDto } from './dto/get-user-paginated.dto';
 
 @Controller('dashboard')
 @UseGuards(AuthGuard, RolesGuard)
@@ -27,5 +28,16 @@ export class DashboardController {
   @Get('users-per-month')
   async getUsersPerMonth() {
     return this.dashboardService.getTotalUserPerMonth();
+  }
+
+  // dashboard.controller.ts
+  @Roles(Role.Admin)
+  @Get('users')
+  async getUserPaginated(@Query() getUserPaginatedData: GetUserPaginatedDto) {
+    return this.dashboardService.getUserPaginated(
+      getUserPaginatedData.limit,
+      getUserPaginatedData.offset,
+      getUserPaginatedData.status,
+    );
   }
 }
