001e3b9a0473bb434369e354fb56782b97044133
VoidKeishi <phananha4@gmail.com>
2024-12-20 02:27:21 +0700

    fix: Cookie secure for HTTPS

--- connext/src/common/constants/jwt.constant.ts
+++ connext/src/common/constants/jwt.constant.ts
@@ -1,5 +1,5 @@
 export const ACCESS_TOKEN_EXPIRE = '30d';
 export const REFRESH_TOKEN_EXPIRE = '120d';
-export const IS_COOKIE_SECURE = false;
+export const IS_COOKIE_SECURE = true;
 export const COOKIE_ACCESS_TOKEN_MAX_AGE = 30 * 24 * 60 * 60 * 1000;
 export const COOKIE_REFRESH_TOKEN_MAX_AGE = 120 * 24 * 60 * 60 * 1000;
